960eb3dbb75e5d12074c757f657c4ba4fd690755
Lorenzo Durante <lolodurante2003@gmail.com>
2024-08-14 21:18:02 -0300

    minor changes

--- app/experiencia/page.tsx
+++ app/experiencia/page.tsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function Page() {
     return (
         <div>
-            <span className="text-neutral-100 text-lg">
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
                 Experiencia laboral
-            </span>
+            </h1>
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 pt-4">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 May. 2022 Jul 2024
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                Commerce Frontend Developer en Valtech
+                Commerce Frontend Developer, Valtech
               </p>
             </div>
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 my-4">
@@ -19,12 +19,12 @@ export default function Page() {
                 Feb. 2021 May. 2022
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                Frontend tutor en Coderhouse
+                Frontend tutor, Coderhouse
               </p>
               </div>
-            <span className="text-neutral-100 text-lg">
+            <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
                 Educación
-            </span>
+            </h1>
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 pt-4">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 2024 - 2029
